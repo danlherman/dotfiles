c93e7d91ab4b524a18cd87bd285977576294e9fc
Dan Herman <dherman@globaldebtregistry.com>
2016-05-17 11:00:13 -0400

    updated aliases for alias editing

--- .aliases.sh
+++ .aliases.sh
@@ -4,6 +4,7 @@ alias ll='ls -lh'
 alias be='bundle exec'
 alias vi='vim'
 
+# Links to common directories
 alias c='cd ~/gdr/projects/'
 alias mpl='cd ~/gdr/projects/mpl'
 alias sub='cd ~/gdr/projects/subscriber_portal'
@@ -11,3 +12,6 @@ alias doc='cd ~/gdr/projects/doc_control_client'
 alias dot='cd ~/dotfiles'
 # alias dl='cd ~/gdr/projects/'
 
+# Easy editing of aliases file
+alias ea='vim ~/.aliases.sh'
+alias sa='source ~/.aliases.sh'
